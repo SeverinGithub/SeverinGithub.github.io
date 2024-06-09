ead50e06d7ab0a8aa9dc4aa19d1139e5df61382d
SeverinGithub <159033816+SeverinGithub@users.noreply.github.com>
2024-06-09 16:10:19 +0200

    add new v

--- cash-flow/assets/js/index.js
+++ cash-flow/assets/js/index.js
@@ -1,8 +1,43 @@
 var totalMoney = 0;
 document.getElementById("mainBankStatus").innerHTML = totalMoney;
 
+var bankMoney = 0;
+document.getElementById("bankAccountStatus").innerHTML = bankMoney;
+
+var cashMoney = 0;
+document.getElementById("cashStatus").innerHTML = cashMoney
+
 function addBankMoney() {
-    console.log("add 100 Money");
-    totalMoney += 100;
+    console.log("add Bank Money");
     document.getElementById("mainBankStatus").innerHTML = totalMoney;
+    document.getElementById("bankAccountStatus").innerHTML = bankMoney;
+    
+}
+
+function addCashMoney() {
+    console.log("add 100 Cash");
+    totalMoney += 100;
+    cashMoney += 100;
+    document.getElementById("mainBankStatus").innerHTML = totalMoney;
+    document.getElementById("cashStatus").innerHTML = cashMoney;
+}
+
+function subtraktCashMoney() {
+    console.log("subtrakt 100 Cash")
+    totalMoney -= 100;
+    cashMoney -= 100;
+    document.getElementById("mainBankStatus").innerHTML = totalMoney;
+    document.getElementById("cashStatus").innerHTML = cashMoney;
+}
+
+function subtraktBankMoney() {
+    console.log("subtrakt 100 Money")
+    totalMoney -= 100;
+    bankMoney -= 100;
+    document.getElementById("mainBankStatus").innerHTML = totalMoney;
+    document.getElementById("bankAccountStatus").innerHTML = bankMoney;
+}
+
+function saveBankInput() {
+    console.log("saved money input")
 }
