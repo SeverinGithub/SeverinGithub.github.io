0157128e22b23acece80ceb143391dd096a77b19
SeverinGithub <159033816+SeverinGithub@users.noreply.github.com>
2024-06-11 16:04:26 +0200

    add weather-app

--- cash-flow/assets/js/index.js
+++ cash-flow/assets/js/index.js
@@ -1,43 +1,92 @@
-var totalMoney = 0;
-document.getElementById("mainBankStatus").innerHTML = totalMoney;
+// var totalMoney = 0;
+// document.getElementById("mainBankStatus").innerHTML = totalMoney;
 
-var bankMoney = 0;
-document.getElementById("bankAccountStatus").innerHTML = bankMoney;
+// var bankMoney = 0;
+// document.getElementById("bankAccountStatus").innerHTML = bankMoney;
 
-var cashMoney = 0;
-document.getElementById("cashStatus").innerHTML = cashMoney
+// var cashMoney = 0;
+// document.getElementById("cashStatus").innerHTML = cashMoney;
 
-function addBankMoney() {
-    console.log("add Bank Money");
-    document.getElementById("mainBankStatus").innerHTML = totalMoney;
-    document.getElementById("bankAccountStatus").innerHTML = bankMoney;
-    
+// var bankMoneyInput = [];
+// document.getElementById("newMoneyIncome").innerHtml = bankMoneyInput;
+
+// function addBankMoney() {
+//     console.log("add Bank Money");
+//     document.getElementById("mainBankStatus").innerHTML = totalMoney;
+//     document.getElementById("bankAccountStatus").innerHTML = bankMoney;
+// }
+
+// function addCashMoney() {
+//     console.log("add 100 Cash");
+//     totalMoney += 100;
+//     cashMoney += 100;
+//     document.getElementById("mainBankStatus").innerHTML = totalMoney;
+//     document.getElementById("cashStatus").innerHTML = cashMoney;
+// }
+
+// function subtraktCashMoney() {
+//     console.log("subtrakt 100 Cash");
+//     totalMoney -= 100;
+//     cashMoney -= 100;
+//     document.getElementById("mainBankStatus").innerHTML = totalMoney;
+//     document.getElementById("cashStatus").innerHTML = cashMoney;
+// }
+
+// function subtraktBankMoney() {
+//     console.log("subtrakt 100 Money");
+//     totalMoney -= 100;
+//     bankMoney -= 100;
+//     document.getElementById("mainBankStatus").innerHTML = totalMoney;
+//     document.getElementById("bankAccountStatus").innerHTML = bankMoney;
+// }
+
+
+// -----------------------------------
+// MODEL
+
+var cash = 0
+var bank = 0
+
+function addCash(newMoney) {
+    cash += newMoney
+}
+function addBank(newMoney) {
+    console.log("New Income + " + newMoney);
+    bank += newMoney
 }
 
-function addCashMoney() {
-    console.log("add 100 Cash");
-    totalMoney += 100;
-    cashMoney += 100;
-    document.getElementById("mainBankStatus").innerHTML = totalMoney;
-    document.getElementById("cashStatus").innerHTML = cashMoney;
+function getTotal() {
+    return cash + bank
 }
 
-function subtraktCashMoney() {
-    console.log("subtrakt 100 Cash")
-    totalMoney -= 100;
-    cashMoney -= 100;
-    document.getElementById("mainBankStatus").innerHTML = totalMoney;
-    document.getElementById("cashStatus").innerHTML = cashMoney;
+//addCash(200)
+
+
+// -----------------------------------
+// VIEW
+
+function redisplay() {
+    console.log("redisplay")
+    document.getElementById("mainCashStatus").innerHTML = cash;// + "&nbsp;&euro;"
+    document.getElementById("mainBankStatus").innerHTML = bank;
+    document.getElementById("mainTotalStatus").innerHTML = getTotal();
 }
 
-function subtraktBankMoney() {
-    console.log("subtrakt 100 Money")
-    totalMoney -= 100;
-    bankMoney -= 100;
-    document.getElementById("mainBankStatus").innerHTML = totalMoney;
-    document.getElementById("bankAccountStatus").innerHTML = bankMoney;
-}
+
+// -----------------------------------
+// CONTROLLER
 
 function saveBankInput() {
-    console.log("saved money input")
+    var str = document.getElementById("newMoneyIncome").value
+    console.log("saved money input" + str);
+    var flt = parseFloat(str)
+    console.log(" float: " + flt)
+    addBank(flt)
+    redisplay()
+}
+
+function saveCashInput() {
+    console.log("saved money input");
+    addCash(parseFloat(document.getElementById("newCashMoneyIncome").value))
+    redisplay()
 }
