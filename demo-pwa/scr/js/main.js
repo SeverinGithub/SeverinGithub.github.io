f128983636a702542d1b4434269362ac868bff92
SeverinGithub <159033816+SeverinGithub@users.noreply.github.com>
2024-06-25 17:15:05 +0200

    delite button; fix NAN bug

--- demo-pwa/scr/js/main.js
+++ demo-pwa/scr/js/main.js
@@ -1,18 +1,17 @@
 var totalclicks = 0
 
 function addAClick() {
-    totalclicks += 1;
-    document.getElementById("totalClicks").innerHTML = totalclicks;
-    localStorage.setItem("localTotalClicks", totalclicks);
+        totalclicks += 1;
+        document.getElementById("totalClicks").innerHTML = totalclicks;
+        localStorage.setItem("localTotalClicks", totalclicks);
 }
 
 window.onload = (event) => {
+    if (localStorage.getItem("localTotalClicks", NaN)){
+        totalclicks = 0;
+        localStorage.setItem("localTotalClicks", 0);
+        document.getElementById("totalClicks").innerHTML = totalclicks;
+    }
     totalclicks = parseInt(localStorage.getItem("localTotalClicks", totalclicks));
     document.getElementById("totalClicks").innerHTML = totalclicks;
 };
-
-function refreshClicks() {
-    localStorage.setItem("localTotalClicks", 0);
-    var totalclicks = 0;
-    document.getElementById("totalClicks").innerHTML = totalclicks;
-}
