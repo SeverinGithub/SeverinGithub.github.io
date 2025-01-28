ab17bc4af12d289a95e15e1cc3f1e4e407a19fbd
SeverinGithub <159033816+SeverinGithub@users.noreply.github.com>
2025-01-28 21:28:08 +0100

    new version 0.5.0

--- cash-flow/src/framework/card-list.js
+++ cash-flow/src/framework/card-list.js
@@ -1,12 +1,45 @@
 
 
 {#CARDLIST#:
+<ol class="list-group marginsetter list-group-numbered">
 
-  <ul class="list-group marginsetter list-group-flush">
-  <li id="listOb1" class="list-group-item"></li>
-  <li id="listOb2" class="list-group-item"></li>
-  <li id="listOb3" class="list-group-item"></li>
-  </ul>
+<li class="list-group-item d-flex justify-content-between align-items-start">
+  <div class="ms-2 me-auto">
+    <div class="fw-bold">Bitcoin</div>
+    01.01.2024
+  </div>
+  <span class="badge text-bg-danger rounded-pill">-1000</span>
+</li>
+<li class="list-group-item d-flex justify-content-between align-items-start">
+  <div class="ms-2 me-auto">
+    <div class="fw-bold">Hose</div>
+    06.01.2024
+  </div>
+  <span class="badge text-bg-danger rounded-pill">-25</span>
+</li>
+<li class="list-group-item d-flex justify-content-between align-items-start">
+  <div class="ms-2 me-auto">
+    <div class="fw-bold">January</div>
+    02.02.2024
+  </div>
+  <span class="badge text-bg-success rounded-pill">+583</span>
+</li>
+
+
+</ol>
+
+:##}
+
+
+
+
+
+
+{/* <ul class="list-group marginsetter list-group-flush">
+<li id="listOb1" class="list-group-item"></li>
+<li id="listOb2" class="list-group-item"></li>
+<li id="listOb3" class="list-group-item"></li>
+</ul>
 
 <script>
 let text = '{"listorders":[' +
@@ -24,7 +57,4 @@ obj.listorders[1].date + " / " + obj.listorders[1].ordervalue;
 document.getElementById("listOb3").innerHTML =
 obj.listorders[2].date + " / " + obj.listorders[2].ordervalue;
 
-</script>
-
-:##}
-
+</script> */}
