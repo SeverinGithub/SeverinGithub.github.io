196cf11fa402643e71d48a63cba82a1cf78d816c
SeverinGithub <159033816+SeverinGithub@users.noreply.github.com>
2025-02-03 15:58:35 +0100

    new app

--- cash-flow/src/framework/card-list.js
+++ cash-flow/src/framework/card-list.js
@@ -30,31 +30,3 @@
 
 :##}
 
-
-
-
-
-
-{/* <ul class="list-group marginsetter list-group-flush">
-<li id="listOb1" class="list-group-item"></li>
-<li id="listOb2" class="list-group-item"></li>
-<li id="listOb3" class="list-group-item"></li>
-</ul>
-
-<script>
-let text = '{"listorders":[' +
-'{"date":"02.12.2024","ordervalue":"120€" },' +
-'{"date":"11.03.2025","ordervalue":"220€" },' +
-'{"date":"12.03.2025","ordervalue":"-340€" }]}';
-
-const obj = JSON.parse(text);
-document.getElementById("listOb1").innerHTML =
-obj.listorders[0].date + " / " + obj.listorders[0].ordervalue;
-
-document.getElementById("listOb2").innerHTML =
-obj.listorders[1].date + " / " + obj.listorders[1].ordervalue;
-
-document.getElementById("listOb3").innerHTML =
-obj.listorders[2].date + " / " + obj.listorders[2].ordervalue;
-
-</script> */}
--- cash-flow/src/framework/navbar.js
+++ cash-flow/src/framework/navbar.js
@@ -10,11 +10,3 @@
           
     </nav>
 :##}
-
-
-
-
-
-{/* <div href="https://severingithub.github.io/cash-flow/build/" type="button" class="btn btn-danger g-col-4">Home</div>
-                  <div type="button" class="btn btn-danger g-col-4">List</div>
-                  <div type="button" class="btn btn-danger rigtmargin g-col-4">Graphic</div> */}
