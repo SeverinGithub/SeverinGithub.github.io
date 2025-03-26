f71f5a6173b5338ced72bc2c78edc91801cf6a06
SeverinGithub <159033816+SeverinGithub@users.noreply.github.com>
2025-03-26 21:42:05 +0100

    new stuff

---
+++ networther/service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'cashflow-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    'index.html',
+    'manifest.json',
+    'icon-192x192.png',
+    'icon-512x512.png'
+];
+
+// Install service worker and cache all assets
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// Activate and clean up old caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then(response => {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    });
+            })
+    );
+}); 
