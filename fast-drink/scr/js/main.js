1c89bba71e03a396cbfce67982465dafd60a561b
SeverinGithub <159033816+SeverinGithub@users.noreply.github.com>
2025-04-19 12:47:13 +0200

    new stuff

--- fast-drink/scr/js/main.js
+++ fast-drink/scr/js/main.js
@@ -1,6 +1,29 @@
+function sendMail(subject, body) {
+    var req = new XMLHttpRequest()
+    req.open("GET", "https://monet.schwarz-online.com:8140/luis-delivery-cgi/sendmail.pl?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body))
+    req.onload = function(oEvent) {
+        if (req.responseText.startsWith("E-Mail sent")) {
+            //alert("Erfolg!"); -> toast
+            
+        }
+        else {
+            alert("Fehler!");
+        }
+    };
 
+    req.send(null)
+}
 
+var subject = " ";
+var body = " ";
 
+$(document).ready(function() {
+    sendMail("my subject", "my body")
+})
+
+$("#").off('click').on('click', function() {
+
+});
 
 
 
