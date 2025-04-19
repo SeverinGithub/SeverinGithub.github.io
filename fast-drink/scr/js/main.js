80b1816eeda614d001e1d9e665efaf1d97ab586c
SeverinGithub <159033816+SeverinGithub@users.noreply.github.com>
2025-04-19 12:35:54 +0200

    back to the roots

--- fast-drink/scr/js/main.js
+++ fast-drink/scr/js/main.js
@@ -29,33 +29,5 @@ $("#").off('click').on('click', function() {
 
 
 
-//  const themeToggle = document.getElementById('themeToggle');
-//  const toggleInput = themeToggle.querySelector('input');
-//  const toggleLabel = themeToggle.querySelector('.toggle-label');
-//  const html = document.documentElement;
 
-//  function initTheme() {
-//      const savedTheme = localStorage.getItem('theme') || 'dark';
-//      html.setAttribute('data-bs-theme', savedTheme);
-//      toggleInput.checked = savedTheme === 'light';
-//      updateLabel(savedTheme);
-//  }
-
-//  function updateLabel(theme) {
-//      toggleLabel.textContent = theme === 'dark' ? 'Dark' : 'Light';
-//  }
-
-//  function toggleTheme() {
-//      const currentTheme = html.getAttribute('data-bs-theme');
-//      const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-     
-//      html.setAttribute('data-bs-theme', newTheme);
-//      localStorage.setItem('theme', newTheme);
-//      toggleInput.checked = newTheme === 'light';
-//      updateLabel(newTheme);
-//  }
-
-//  initTheme();
-
-//  toggleInput.addEventListener('change', toggleTheme);
 
