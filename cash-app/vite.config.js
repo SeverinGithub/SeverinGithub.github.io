32b3c44c044dc0e995d130e00635f7cd9698d535
SeverinGithub <159033816+SeverinGithub@users.noreply.github.com>
2026-05-02 12:44:12 +0200

    new

--- cash-app/vite.config.js
+++ cash-app/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  // GitHub Pages project path
+  base: '/cash-app/',
   plugins: [react()],
 })
